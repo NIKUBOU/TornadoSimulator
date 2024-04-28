d127dbf499cf8aa1461ca1b03c9faf8cac6b59fc
caleb.secrest <caleb.secrest@mymail.champlain.edu>
2024-04-28 09:56:51 -0400

    House & NPC Adjustments

--- Assets/Scripts/NPCMovement.cs
+++ Assets/Scripts/NPCMovement.cs
@@ -14,6 +14,10 @@ public class NPCMovement : MonoBehaviour
     [SerializeField] private List<Transform> firstRow;
     [SerializeField] private List<Transform> secondRow;
     [SerializeField] private List<Transform> thirdRow;
+    [SerializeField] private List<Transform> fourthRow;
+    [SerializeField] private List<Transform> fifthRow;
+    [SerializeField] private List<Transform> sixthRow;
+    [SerializeField] private List<Transform> seventhRow;
 
     private Transform currentTarget;
     private bool crying = false;
@@ -84,6 +88,42 @@ public class NPCMovement : MonoBehaviour
                 break;
             }
         }
+
+        foreach (Transform t in fourthRow)
+        {
+            if (t == transform)
+            {
+                SetTargetFromRowFour(fourthRow.IndexOf(t));
+                break;
+            }
+        }
+
+        foreach (Transform t in fifthRow)
+        {
+            if (t == transform)
+            {
+                SetTargetFromRowFive(fifthRow.IndexOf(t));
+                break;
+            }
+        }
+
+        foreach (Transform t in sixthRow)
+        {
+            if (t == transform)
+            {
+                SetTargetFromRowSix(sixthRow.IndexOf(t));
+                break;
+            }
+        }
+
+        foreach (Transform t in seventhRow)
+        {
+            if (t == transform)
+            {
+                SetTargetFromRowSeven(seventhRow.IndexOf(t));
+                break;
+            }
+        }
     }
 
     private void SetTargetFromRowOne(int index)
@@ -97,7 +137,7 @@ public class NPCMovement : MonoBehaviour
             }
             else if (ranNum == 1)
             {
-                currentTarget = secondRow[index];
+                currentTarget = secondRow[1];
             }
         }
         else if (index == 1)
@@ -113,7 +153,7 @@ public class NPCMovement : MonoBehaviour
             }
             else if (ranNum == 2)
             {
-                currentTarget = secondRow[index];
+                currentTarget = thirdRow[4];
             }
         }
         else if (index == 2)
@@ -125,7 +165,7 @@ public class NPCMovement : MonoBehaviour
             }
             else if (ranNum == 1)
             {
-                currentTarget = secondRow[index];
+                currentTarget = thirdRow[5];
             }
         }
     }
@@ -134,18 +174,46 @@ public class NPCMovement : MonoBehaviour
     {
         if (index == 0)
         {
-            int ranNum = Random.Range(0, 3);
+            int ranNum = Random.Range(0, 2);
             if (ranNum == 0)
             {
                 currentTarget = secondRow[index + 1];
             }
             else if (ranNum == 1)
             {
-                currentTarget = firstRow[index];
+                currentTarget = thirdRow[1];
+            }
+        }
+        else if (index == 1)
+        {
+            int ranNum = Random.Range(0, 3);
+            if (ranNum == 0)
+            {
+                currentTarget = firstRow[0];
+            }
+            else if (ranNum == 1)
+            {
+                currentTarget = secondRow[index - 1];
             }
             else if (ranNum == 2)
             {
-                currentTarget = thirdRow[index];
+                currentTarget = thirdRow[3];
+            }
+        }
+    }
+
+    private void SetTargetFromRowThree(int index)
+    {
+        if (index == 0)
+        {
+            int ranNum = Random.Range(0, 2);
+            if (ranNum == 0)
+            {
+                currentTarget = thirdRow[index + 1];
+            }
+            else if (ranNum == 1)
+            {
+                currentTarget = fourthRow[0];
             }
         }
         else if (index == 1)
@@ -153,19 +221,19 @@ public class NPCMovement : MonoBehaviour
             int ranNum = Random.Range(0, 4);
             if (ranNum == 0)
             {
-                currentTarget = secondRow[index + 1];
+                currentTarget = thirdRow[index + 1];
             }
             else if (ranNum == 1)
             {
-                currentTarget = secondRow[index - 1];
+                currentTarget = thirdRow[index - 1];
             }
             else if (ranNum == 2)
             {
-                currentTarget = firstRow[index];
+                currentTarget = secondRow[0];
             }
             else if (ranNum == 3)
             {
-                currentTarget = thirdRow[index];
+                currentTarget = fourthRow[1];
             }
         }
         else if (index == 2)
@@ -173,20 +241,316 @@ public class NPCMovement : MonoBehaviour
             int ranNum = Random.Range(0, 3);
             if (ranNum == 0)
             {
-                currentTarget = secondRow[index - 1];
+                currentTarget = thirdRow[index + 1];
             }
             else if (ranNum == 1)
             {
-                currentTarget = firstRow[index];
+                currentTarget = thirdRow[index - 1];
             }
             else if (ranNum == 2)
             {
-                currentTarget = thirdRow[index];
+                currentTarget = fourthRow[2];
+            }
+        }
+        else if (index == 3)
+        {
+            int ranNum = Random.Range(0, 4);
+            if (ranNum == 0)
+            {
+                currentTarget = thirdRow[index + 1];
+            }
+            else if (ranNum == 1)
+            {
+                currentTarget = thirdRow[index - 1];
+            }
+            else if (ranNum == 2)
+            {
+                currentTarget = secondRow[1];
+            }
+            else if (ranNum == 3)
+            {
+                currentTarget = fourthRow[3];
+            }
+        }
+        else if (index == 4)
+        {
+            int ranNum = Random.Range(0, 4);
+            if (ranNum == 0)
+            {
+                currentTarget = thirdRow[index + 1];
+            }
+            else if (ranNum == 1)
+            {
+                currentTarget = thirdRow[index - 1];
+            }
+            else if (ranNum == 2)
+            {
+                currentTarget = firstRow[1];
+            }
+            else if (ranNum == 3)
+            {
+                currentTarget = fourthRow[4];
+            }
+        }
+        else if (index == 5)
+        {
+            int ranNum = Random.Range(0, 3);
+            if (ranNum == 0)
+            {
+                currentTarget = thirdRow[index + 1];
+            }
+            else if (ranNum == 1)
+            {
+                currentTarget = thirdRow[index - 1];
+            }
+            else if (ranNum == 2)
+            {
+                currentTarget = firstRow[2];
+            }
+        }
+        else if (index == 6)
+        {
+            int ranNum = Random.Range(0, 2);
+            if (ranNum == 0)
+            {
+                currentTarget = fourthRow[5];
+            }
+            else if (ranNum == 1)
+            {
+                currentTarget = thirdRow[index - 1];
             }
         }
     }
 
-    private void SetTargetFromRowThree(int index)
+    private void SetTargetFromRowFour(int index)
+    {
+        if (index == 0)
+        {
+            int ranNum = Random.Range(0, 3);
+            if (ranNum == 0)
+            {
+                currentTarget = thirdRow[0];
+            }
+            else if (ranNum == 1)
+            {
+                currentTarget = fourthRow[index + 1];
+            }
+            else if (ranNum == 2)
+            {
+                currentTarget = sixthRow[0];
+            }
+        }
+        else if (index == 1)
+        {
+            int ranNum = Random.Range(0, 4);
+            if (ranNum == 0)
+            {
+                currentTarget = fourthRow[index + 1];
+            }
+            else if (ranNum == 1)
+            {
+                currentTarget = fourthRow[index - 1];
+            }
+            else if (ranNum == 2)
+            {
+                currentTarget = thirdRow[1];
+            }
+            else if (ranNum == 3)
+            {
+                currentTarget = fifthRow[0];
+            }
+        }
+        else if (index == 2)
+        {
+            int ranNum = Random.Range(0, 4);
+            if (ranNum == 0)
+            {
+                currentTarget = fourthRow[index + 1];
+            }
+            else if (ranNum == 1)
+            {
+                currentTarget = fourthRow[index - 1];
+            }
+            else if (ranNum == 2)
+            {
+                currentTarget = thirdRow[2];
+            }
+            else if (ranNum == 2)
+            {
+                currentTarget = fifthRow[1];
+            }
+        }
+        else if (index == 3)
+        {
+            int ranNum = Random.Range(0, 4);
+            if (ranNum == 0)
+            {
+                currentTarget = fourthRow[index + 1];
+            }
+            else if (ranNum == 1)
+            {
+                currentTarget = fourthRow[index - 1];
+            }
+            else if (ranNum == 2)
+            {
+                currentTarget = thirdRow[3];
+            }
+            else if (ranNum == 3)
+            {
+                currentTarget = fifthRow[2];
+            }
+        }
+        else if (index == 4)
+        {
+            int ranNum = Random.Range(0, 4);
+            if (ranNum == 0)
+            {
+                currentTarget = fourthRow[index + 1];
+            }
+            else if (ranNum == 1)
+            {
+                currentTarget = fourthRow[index - 1];
+            }
+            else if (ranNum == 2)
+            {
+                currentTarget = thirdRow[4];
+            }
+            else if (ranNum == 3)
+            {
+                currentTarget = fifthRow[3];
+            }
+        }
+        else if (index == 5)
+        {
+            int ranNum = Random.Range(0, 2);
+            if (ranNum == 0)
+            {
+                currentTarget = thirdRow[6];
+            }
+            else if (ranNum == 1)
+            {
+                currentTarget = fourthRow[index - 1];
+            }
+        }
+    }
+
+    private void SetTargetFromRowFive(int index)
+    {
+        if (index == 0)
+        {
+            int ranNum = Random.Range(0, 2);
+            if (ranNum == 0)
+            {
+                currentTarget = fifthRow[index + 1];
+            }
+            else if (ranNum == 1)
+            {
+                currentTarget = fourthRow[1];
+            }
+        }
+        else if (index == 1)
+        {
+            int ranNum = Random.Range(0, 4);
+            if (ranNum == 0)
+            {
+                currentTarget = fifthRow[index + 1];
+            }
+            else if (ranNum == 1)
+            {
+                currentTarget = fifthRow[index - 1];
+            }
+            else if (ranNum == 2)
+            {
+                currentTarget = fourthRow[2];
+            }
+            else if (ranNum == 3)
+            {
+                currentTarget = sixthRow[1];
+            }
+        }
+        else if (index == 2)
+        {
+            int ranNum = Random.Range(0, 4);
+            if (ranNum == 0)
+            {
+                currentTarget = fifthRow[index + 1];
+            }
+            else if (ranNum == 1)
+            {
+                currentTarget = fifthRow[index - 1];
+            }
+            else if (ranNum == 2)
+            {
+                currentTarget = fourthRow[3];
+            }
+            else if (ranNum == 3)
+            {
+                currentTarget = sixthRow[2];
+            }
+        }
+        else if (index == 3)
+        {
+            int ranNum = Random.Range(0, 3);
+            if (ranNum == 0)
+            {
+                currentTarget = fifthRow[index + 1];
+            }
+            else if (ranNum == 1)
+            {
+                currentTarget = fifthRow[index - 1];
+            }
+            else if (ranNum == 2)
+            {
+                currentTarget = fourthRow[3];
+            }
+        }
+    }
+
+    private void SetTargetFromRowSix(int index)
+    {
+        if (index == 0)
+        {
+            int ranNum = Random.Range(0, 2);
+            if (ranNum == 0)
+            {
+                currentTarget = thirdRow[index + 1];
+            }
+            else if (ranNum == 1)
+            {
+                currentTarget = secondRow[index];
+            }
+        }
+        else if (index == 1)
+        {
+            int ranNum = Random.Range(0, 3);
+            if (ranNum == 0)
+            {
+                currentTarget = thirdRow[index + 1];
+            }
+            else if (ranNum == 1)
+            {
+                currentTarget = thirdRow[index - 1];
+            }
+            else if (ranNum == 2)
+            {
+                currentTarget = secondRow[index];
+            }
+        }
+        else if (index == 2)
+        {
+            int ranNum = Random.Range(0, 2);
+            if (ranNum == 0)
+            {
+                currentTarget = thirdRow[index - 1];
+            }
+            else if (ranNum == 1)
+            {
+                currentTarget = secondRow[index];
+            }
+        }
+    }
+
+    private void SetTargetFromRowSeven(int index)
     {
         if (index == 0)
         {
